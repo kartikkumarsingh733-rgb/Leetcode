4dacd2f870df049f2ca39c96f5fb2ecbff509a8c
kartikkumarsingh733-rgb <kartikkumarsingh733@gmail.com>
2026-07-18 17:17:57 +0530

    Time: 0 ms (100%), Space: 28.9 MB (94.53%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -1,42 +1,15 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    bool same(TreeNode* p,TreeNode* q){
-        if(p==NULL&&q==NULL) return true;
-
-        if(p!=NULL&&q==NULL||p==NULL&&q!=NULL) return false;
-        if(p->val!=q->val) return false;
-
-        if(same(p->left,q->left)&&same(p->right,q->right)) return true;
-
-        return false;
+    bool same(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+        if (!p || !q) return false;
+        if (p->val != q->val) return false;
+        return same(p->left, q->left) && same(p->right, q->right);
     }
-    void func(TreeNode* root,TreeNode* subtree,bool &ans){
-        if(root==NULL) return;
 
-        func(root->left,subtree,ans);
-        if(same(root,subtree)){
-            ans=true;
-            return ;
-        }
-        func(root->right,subtree,ans);
-
-        return  ;
-    }
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        bool ans = false;
-        func(root,subRoot,ans);
-
-        return ans;
+        if (!root) return false;
+        if (same(root, subRoot)) return true;
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
     }
 };
