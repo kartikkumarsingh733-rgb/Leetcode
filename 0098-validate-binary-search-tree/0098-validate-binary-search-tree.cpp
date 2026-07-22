b211ff9c52414aae6f8a2bd55a80d5dbd9e0a20a
kartikkumarsingh733-rgb <kartikkumarsingh733@gmail.com>
2026-07-22 22:31:14 +0530

    Time: 0 ms (100%), Space: 21.9 MB (45.36%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,29 +1,24 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void func(TreeNode* root,vector<int> &ans){
-        if(root==NULL) return ;
-        func(root->left,ans);
-        ans.push_back(root->val);
-        func(root->right,ans);
-    }
-    bool isValidBST(TreeNode* root) {
-        vector<int> ans;
-        func(root,ans);
-        for(int i=1;i<ans.size();i++){
-            if(ans[i]<=ans[i-1]) return false;
-        }
+    bool ans = true;
+    long long prev = LLONG_MIN; 
 
-        return true;
+    void func(TreeNode* root) {
+        if (!root || !ans) return;
+
+        func(root->left);
+
+        if (root->val <= prev) {
+            ans = false;
+            return;
+        }
+        prev = root->val;
+
+        func(root->right);
+    }
+
+    bool isValidBST(TreeNode* root) {
+        func(root);
+        return ans;
     }
 };
