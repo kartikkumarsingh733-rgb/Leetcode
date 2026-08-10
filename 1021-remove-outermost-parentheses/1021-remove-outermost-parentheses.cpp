2f21ae2fa47cde77f8a17f575b2ca854a945c9f9
kartikkumarsingh733-rgb <kartikkumarsingh733@gmail.com>
2026-08-10 14:23:33 +0530

    Time: 0 ms (100%), Space: 8.9 MB (81.73%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        string ans = "";
+        string ans="";
         int open = 0;
-        int close = 0;
-        stack<char> stk;
+        int close =0;
+        int bidx = 0;
         for(int i=0;i<s.size();i++){
             char c = s[i];
             if(c=='(') open++;
             if(c==')') close++;
-            if(!stk.empty()&&close==open){
-                int len = stk.size();
-                string temp = "";
-                while((len--)>1){
-                    temp.push_back(stk.top());
-                    stk.pop();
-                }
-                if(stk.size()!=0)stk.pop();
-                reverse(temp.begin(),temp.end());
-                ans+=temp;
-                open = 0;
-                close = 0;
-                continue;
+            if(close==open){
+                ans+= s.substr(bidx+1,i-bidx-1);
+                bidx = i+1;
             }
-            stk.push(c);
         }
+
         return ans;
     }
 };
